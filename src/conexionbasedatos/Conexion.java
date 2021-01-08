1ad61e43eb561448eed092537f645325311359eb
johnp <johnp@DESKTOP-KOGH08M.home>
2021-01-07 19:54:17 -0500

    Commit Inicial

---
+++ src/conexionbasedatos/Conexion.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexionbasedatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author johnp
+ */
+public class Conexion {
+    private Connection conexion;
+    //declaramos los dadtos de conexion
+    private static final String driver = "com.mysql.jdbc.Driver";
+    private static final String user = "userclases";
+    private static final String pass = "12345678$";
+    private static final String url = "jdbc:mysql://localhost:3306/ejercicio? useUnicode=true&use"
+            + " JDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+    public Connection conectarBaseDatos() {
+        //reseteamos null a la conexion de la BD
+        conexion = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //conexion a la BD
+            conexion = (Connection) DriverManager.getConnection(url, user, pass);
+            //comprobacion
+            if (conexion != null) {
+                System.out.println("Conexion exitosa");
+            }
+            //mensaje de error
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return conexion;
+    }
+}
---
+++ src/conexionbasedatos/ConexionBaseDatos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexionbasedatos;
+
+/**
+ *
+ * @author johnp
+ */
+public class ConexionBaseDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Conexion conexion = new Conexion();
+        conexion.conectarBaseDatos();
+    }
+    
+}
