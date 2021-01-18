36cf5b63c7194fae12aec9c33cc0e3281ff8c684
johnp <johnp@DESKTOP-KOGH08M.home>
2021-01-18 08:12:39 -0500

    Commit V2

--- src/com/istloja/vistas/GestionPersona.java
+++ src/com/istloja/vistas/GestionPersona.java
@@ -5,7 +5,10 @@
  */
 package com.istloja.vistas;
 
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,7 +21,8 @@ import javax.swing.JTextField;
  */
 public class GestionPersona extends JFrame{
     
-    private JPanel panel = new JPanel();
+    private JPanel panelTitulo = new JPanel();
+    private JPanel panelCuerpo = new JPanel();
 
     public GestionPersona() {
         //Mostrar Pantalla
@@ -27,17 +31,26 @@ public class GestionPersona extends JFrame{
         this.setTitle("Ventana");
         //Tamanio Inicial de la venta
         this.setSize(400,600);
+//        JPanel panel = new JPanel();
+//        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+      
         //Se agregar un layout distribuir los componentes.
-        panel.setLayout(new FlowLayout());
-        JLabel label = new JLabel("VENTA DE REGISTRO DE PERSONAS.");  
-        JButton button = new JButton();  
-        button.setText("REGISTRAR PERSONAS");  
-        panel.add(label);
-        panel.add(button);
+        panelTitulo.setLayout(new FlowLayout());
+        JLabel label = new JLabel("Registro de personas.");   
+        panelTitulo.add(label);
         //Agrege el panel al Jframe
-        this.add(panel);
+//        panel.add(panelTitulo);
+        panelCuerpo.setLayout(new GridLayout(4,2));
+        panelCuerpo.add(new JLabel("cedula:"));
+        panelCuerpo.add(new JTextField("John Patricio"));
+        panelCuerpo.add(new JLabel("cedula:"));
+  
+
+//        panel.add(panelCuerpo);
+        this.add(panelCuerpo);
         //Centrar la pantalla
         this.setLocationRelativeTo(null);  
+        this.setSize(new Dimension(1024, 768));
         //Fin al programa.
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
        
